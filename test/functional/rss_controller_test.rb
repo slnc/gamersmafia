10f9fcf0046b37660d4bf2c27d758396632575b5
slnc <slnc@5cc16f97-7304-0410-b65f-8167f678e89c>
2009-01-28 19:09:10 +0000

    * quitado código de rss cola_moderacion ya que no se usa
    git-svn-id: https://balrog.slnc.net/gamersmafia/trunk@3651 5cc16f97-7304-0410-b65f-8167f678e89c

--- test/functional/rss_controller_test.rb
+++ test/functional/rss_controller_test.rb
@@ -12,30 +12,9 @@ class RssControllerTest < Test::Unit::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-  #assert_valid_feed [ :noticias ]
-
-  #def test_should_load_noticias
-  #  get :noticias
-  #  assert_response :success
-  #  assert_template 'rss/noticias'
-  #end
-  #
   def test_rss_noticias
     get :noticias
     assert_response :success
-    assert_valid_feed2
-  end
-
-  def test_should_not_access_moderation_queue_if_no_secret_given
-    assert_raises(AccessDenied) do
-      get :cola_moderacion
-    end
-  end
-
-  def test_should_access_moderation_queue_if_given_secret
-    get :cola_moderacion, :secret => User.find(1).secret
-    assert_response :success
-    assert_template 'rss/cola_moderacion'
     assert_valid_feed2
   end
 end
