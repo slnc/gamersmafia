3813b4fd724de62ff77a15ef37f7630cfa43e0f3
slnc <s@slnc.me>
2009-02-28 20:15:42 +0100

    corregidos varios bugs

--- test/unit/advertiser_test.rb
+++ test/unit/advertiser_test.rb
@@ -6,7 +6,7 @@ class AdvertiserTest < Test::Unit::TestCase
   def test_del_roles
     adv = Advertiser.new(:name => 'foo adv', :due_on_day => 15, :email => 'money@me.com')
     assert adv.save
-    ur = UsersRole.new(:role => 'Advertiser', :user_id => 1, :role_data => adv.id)
+    ur = UsersRole.new(:role => 'Advertiser', :user_id => 1, :role_data => adv.id.to_s)
     assert ur.save
     adv.destroy
     assert_nil UsersRole.find_by_id(ur.id)
