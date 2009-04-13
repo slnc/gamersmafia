7db8e0f07d632b9e0a5067b5681053ca98fc5271
slnc <s@slnc.me>
2009-04-13 12:09:20 +0200

    más limpieza de RAILS_ROOT y script/ inócuas

---
+++ script/dump_schema.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Guarda la definición de la BBDD en el archivo .sql de creación
+# No usamos el mecanismo de Rails de schema.rb porque no guarda índices,
+# foreign keys y otras constraints que nosotros usamos
+pg_dump -xsOR gamersmafia > ../db/create.sql
