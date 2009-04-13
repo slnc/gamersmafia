8b5b9ff8308c0022064d00ca39a1a57e9136a644
slnc <s@slnc.me>
2009-04-13 12:03:46 +0200

    Movido directorio donde se guarda el archivo mode (y ordenados otros archivos)
    Cambiado de RAILS_ROOT/mode a RAILS_ROOT/config/mode para tener
    RAILS_ROOT más ordenado.

--- config/app_config.rb
+++ config/app_config.rb
@@ -5,7 +5,7 @@ LINUX = 0
 WINDOWS = 1
 
 # load custom config
-mode = File.exists?("#{RAILS_ROOT}/mode") ? File.open("#{RAILS_ROOT}/mode").read.strip : 'doll2'
+mode = File.exists?("#{RAILS_ROOT}/config/mode") ? File.open("#{RAILS_ROOT}/config/mode").read.strip : 'doll2'
 mode = 'test' if RAILS_ENV == 'test'
 nconfig = OpenStruct.new(YAML::load(ERB.new((IO.read("#{RAILS_ROOT}/config/app.yml"))).result))
 env_config = nconfig.send(mode)
