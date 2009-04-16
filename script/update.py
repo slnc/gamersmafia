deb93c922377b1e06a443c63de014c774502544b
slnc <s@slnc.me>
2009-04-16 23:04:22 +0200

    corregido un bug en nueva lógica de actualización de update.py

--- script/update.py
+++ script/update.py
@@ -59,12 +59,12 @@ def send_changelog_email():
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
     
-    open('%s/config/PREV_REVISION' % wc_path_clean, 'w').write('%s' % cur)
     # Hacemos todo esto simplemente para guardar cuándo hacemos una nueva release
     # TODO deberíamos generar tags para no enviar emails de cambios con el id de hash sino con algo como
     # 2009.<num_de_actualizacion_anual>
     # estamos en deploy
     os.popen('git checkout production')
+    open('%s/config/PREV_REVISION' % wc_path_clean, 'w').write('%s' % cur)
     os.popen('git add config/PREV_REVISION')
     os.popen('git commit -m "new deployment: %s"' % cur)
     os.popen('git push')
