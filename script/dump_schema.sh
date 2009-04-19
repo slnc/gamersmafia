842de53b115e8080681a769b424fa98ba6158991
slnc <s@slnc.me>
2009-04-19 18:53:53 +0200

    ya no damos emblema al okupa (máx tiempo en la web)ya que siempre genera problemas

--- script/dump_schema.sh
+++ script/dump_schema.sh
@@ -2,4 +2,4 @@
 # Guarda la definición de la BBDD en el archivo .sql de creación
 # No usamos el mecanismo de Rails de schema.rb porque no guarda índices,
 # foreign keys y otras constraints que nosotros usamos
-pg_dump -xsOR gamersmafia > ../db/create.sql
+pg_dump -xsOR gamersmafia > db/create.sql
