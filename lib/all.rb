255297a9a078db028140df1e9341cde0574da4af
slnc <s@slnc.me>
2009-04-25 17:09:18 +0200

    corregido el problema con las fechas en español

--- lib/all.rb
+++ lib/all.rb
@@ -1,36 +1,5 @@
 load RAILS_ROOT + '/Rakefile'
 
-class Time
-  alias :strftime_nolocale :strftime
-  
-  def strftime(format) # PERF: too slow?
-    format = format.dup
-    format.gsub!(/%a/, Date::GM_ABBR_DAYNAMES[self.wday])
-    format.gsub!(/%A/, Date::GM_DAYNAMES[self.wday])
-    format.gsub!(/%b/, Date::GM_ABBR_MONTHNAMES[self.mon])
-    format.gsub!(/%B/, Date::GM_MONTHNAMES[self.mon])
-    self.strftime_nolocale(format)
-  end
-end
-
-
-# if nil # ruby 1.8.7
-class Date
-  GM_MONTHNAMES = [nil, 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo',
-      'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 
-      'Diciembre']
-  
-  GM_ABBR_MONTHNAMES = [nil, 'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
-      'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec']
-  
-  GM_DAYNAMES = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 
-      'Viernes', 'Sábado']
-  
-  GM_ABBR_DAYNAMES = ['D', 'L', 'M', 'X', 'J', 'V', 'S']
-end
-
-
-
 class ActionController::Caching::Fragments::UnthreadedFileStore
   def write(name, value, options = nil) #:nodoc:
     ensure_cache_path(File.dirname(real_file_path(name)))
