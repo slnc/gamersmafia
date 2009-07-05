f1fc05e4c8c76aadebfbedb9c07a67d7d51fdb12
slnc <s@slnc.me>
2009-07-05 17:45:32 +0200

    añadido nuevo script lightweight para monitorizar a passenger

---
+++ script/pmonitor2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import re
+import time
+import urllib2
+
+def check_full_stack():
+    try:
+        req = urllib2.Request('http://www.gamersmafia.com/')
+        r = urllib2.urlopen(req)
+        body = r.read()
+        if not re.search('google-analytics.com', body): # si no hay error pero no sale urchinTracker es que tb ha habido error
+          print "No urchinTracker found, raising URLError"
+          raise urllib2.URLError
+    except urllib2.URLError, e:
+        print "Error al comprobar el stack completo Apache + Mongrel: %s" % e
+
+
+if __name__ == '__main__':
+    try:
+        check_full_stack()
+    except Exception, e:
+        system('/etc/init.d/apachectl stop')
+        time.sleep(5)
+        system('/etc/init.d/apachectl start')
+
