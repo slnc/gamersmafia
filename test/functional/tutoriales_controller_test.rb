89f97eeadda33d8e494bcf37b49e9f55e030d4ec
slnc <s@slnc.me>
2009-07-08 10:20:42 +0200

    error500 on /tutoriales accesed through BazarPortal

--- test/functional/tutoriales_controller_test.rb
+++ test/functional/tutoriales_controller_test.rb
@@ -5,4 +5,9 @@ class TutorialesControllerTest < ActionController::TestCase
   test_common_content_crud :name => 'Tutorial', :form_vars => {:title => 'footapang', :description => 'bartapang', :main => 'aaa'}, :categories_terms => 19
 
 
+  test "should work on bazar" do
+    @request.host = App.domain_bazar
+    get :index
+    assert_response :success
+  end
 end
