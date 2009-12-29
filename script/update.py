5af9ca86826bf36b079703374b289a722721eda3
slnc <s@slnc.me>
2009-12-29 14:50:26 +0100

    corregido update.py

--- script/update.py
+++ script/update.py
@@ -53,7 +53,7 @@ def send_changelog_email():
     toaddrs = 'slnc@gamersmafia.com'
 
     msg = ("Content-Type: text/plain; charset=UTF-8\r\nSubject: GM actualizada a la versión %s\r\nFrom: %s\r\nTo: %s\r\n\r\n%s" % (cur[0:8], fromaddr, toaddrs, log))
-    server = smtplib.SMTP('mail.gamersmafia.com')
+    server = smtplib.SMTP('gamersmafia.com')
     #server.set_debuglevel(1)
     server.login('nagato.gamersmafia.com', 'megustanlasgalletas')
     server.sendmail(fromaddr, toaddrs, msg)
