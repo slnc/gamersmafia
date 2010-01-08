757f71ad480db64d0c3f3acc6f7b23b1c8787680
slnc <s@slnc.me>
2010-01-08 16:42:54 +0100

    corregido error introducido por commits anteriores

--- app/models/contents_term.rb
+++ app/models/contents_term.rb
@@ -3,4 +3,12 @@ class ContentsTerm < ActiveRecord::Base
   belongs_to :term
   validates_presence_of :content_id, :term_id
   validates_uniqueness_of :content_id, :scope => :term_id
+  
+  def import_mode
+    @_import_mode || false
+  end
+  
+  def set_import_mode
+    @_import_mode = true
+  end
 end
