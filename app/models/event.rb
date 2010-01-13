dc0effe4ee997eb5ee5a884fa8cee771ce889c82
slnc <s@slnc.me>
2010-01-13 17:52:41 +0100

    Refactorizaciones a Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,51 +1,28 @@
 class Event < ActiveRecord::Base
   acts_as_content
   acts_as_categorizable
+  acts_as_tree :order => 'title'
   
   named_scope :top_level, :conditions => 'parent_id IS NULL'
-  
-  has_many :coverages
-  
-  has_and_belongs_to_many :users
-  
-  acts_as_tree :order => 'title'
+  named_scope :current_events, :conditions => "events.starts_on < now() + '2 months'::interval
+                                           AND events.parent_id is null
+                                           AND events.ends_on > now() 
+                                           AND events.id not in (SELECT event_id from competitions)", 
+                               :order => 'starts_on'
   
   has_one :competition_match
   has_one :competition
+  has_many :coverages
+  has_and_belongs_to_many :users
+  
   before_validation :check_website_format
+  
+  validates_format_of :website, :with => Cms::URL_REGEXP_FULL, :if => Proc.new{ |c| c.website.to_s != '' }
   
   def check_website_format
     if self[:website] && self[:website].to_s.strip != '' && !(self[:website] =~ /^http:\/\//)
-      self[:website] = 'http://' << self[:website]
+      self[:website] = "http://#{self[:website]}"
     end
-  end
-  
-  
-  CURRENT_SQL = "events.starts_on < now() + '2 months'::interval
-                     AND events.parent_id is null
-                     AND events.ends_on > now() 
-                     AND events.id not in (SELECT event_id from competitions)"
-                     
-  def self.current(opts={})
-    opts = {:order => 'starts_on', :limit => 7}.merge(opts)
-    conds = CURRENT_SQL
-                     
-    if opts[:conditions]
-      opts[:conditions] << " AND #{conds}"
-    else
-      opts[:conditions] = conds
-    end
-    
-    self.find(:published, opts)
-  end
-  
-  # TODO esto falla ahora mismo para portales distintos de gm_portal
-  def self.intersect_day(t)
-    find(:all, 
-         :conditions => "state = #{Cms::PUBLISHED}
-                     AND id not in (SELECT event_id from competitions where event_id is not null) 
-                     AND parent_id is null
-                     AND date_trunc('day', to_timestamp('#{t.strftime('%Y%m%d%H%M%S')}', 'YYYYMMDDHH24MISS')) BETWEEN date_trunc('day', starts_on) AND date_trunc('day', ends_on)")
   end
   
   
@@ -135,6 +112,4 @@ class Event < ActiveRecord::Base
     self.users.delete(u)
     self.save
   end
-  
-  validates_format_of :website, :with => Cms::URL_REGEXP_FULL, :if => Proc.new{ |c| c.website.to_s != '' }
 end
