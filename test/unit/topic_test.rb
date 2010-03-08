33a715f1fda2136682f6ce2f4a1fc8352d0486ac
slnc <s@slnc.me>
2010-03-07 19:25:51 -0600

    Añadido un test más

--- test/unit/topic_test.rb
+++ test/unit/topic_test.rb
@@ -13,8 +13,24 @@ class TopicTest < ActiveSupport::TestCase
     t2 = Topic.create(:title => 'topiquito2', :user_id => 1, :main => 'foo bar chaz a')
     topics1.link(t1.unique_content)
     topics2.link(t2.unique_content)
+    
+    gm = Term.single_toplevel(:slug => 'gm')
+    gmtopics1 = gm.children.create(:name => 'topics1', :taxonomy => 'TopicsCategory')
+    gmtopics2 = gm.children.create(:name => 'topics2', :taxonomy => 'TopicsCategory')
+    gmt1 = Topic.create(:title => 'topiquito1', :user_id => 1, :main => 'foo bar chaz')
+    gmt2 = Topic.create(:title => 'topiquito2', :user_id => 1, :main => 'foo bar chaz a')
+    gmtopics1.link(gmt1.unique_content)
+    gmtopics2.link(gmt2.unique_content)
+    
+    rt.reload
+    gm.reload
+    
     last_topics = Topic.latest_by_category
-    assert last_topics.include?(t2.id)
-    assert !last_topics.include?(t1.id)
+    
+    assert_equal last_topics[0], gmt2
+    assert !last_topics.include?(gmt1)
+    
+    assert_equal last_topics[1], t2
+    assert !last_topics.include?(t1)
   end
 end
