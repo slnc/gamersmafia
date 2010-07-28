721c5a5069679eb7e5b93ff7dddf8440d4feb249
slnc <s@slnc.me>
2010-07-28 20:59:39 +0100

    Eliminadas referencias a mirror de descargas de nls

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -12,31 +12,31 @@ class DemosController < ArenaController
     sql_conds = []
     if params[:demo]
     %w(demotype pov_type games_mode_id event_id games_version_id games_map_id entity1_local_id entity2_local_id).each do |attr|
-      next unless params[:demo] && params[:demo][attr.to_sym].to_s != ''
-      sql_conds<< "#{attr} = #{params[:demo][attr.to_sym].to_i}"
-    end
-    
-    if params[:demo][:entity].to_s != '' then
-      q = "entity1_external = #{User.connection.quote(params[:demo][:entity])} OR entity2_external = #{User.connection.quote(params[:demo][:entity])}"
-      if params[:demo][:games_mode_id] then
-        gmod = GamesMode.find(params[:demo][:games_mode_id])
-        if gmod.entity_type == Game::ENTITY_USER then
+        next unless params[:demo] && params[:demo][attr.to_sym].to_s != ''
+        sql_conds<< "#{attr} = #{params[:demo][attr.to_sym].to_i}"
+      end
+      
+      if params[:demo][:entity].to_s != '' then
+        q = "entity1_external = #{User.connection.quote(params[:demo][:entity])} OR entity2_external = #{User.connection.quote(params[:demo][:entity])}"
+        if params[:demo][:games_mode_id] then
+          gmod = GamesMode.find(params[:demo][:games_mode_id])
+          if gmod.entity_type == Game::ENTITY_USER then
+            u = User.find_by_login(params[:demo][:entity])
+            q<< " OR entity1_local_id = #{u.id} OR entity2_local_id = #{u.id}"
+          else
+            c = Clan.find(:all, :conditions => ['lower(name) = lower(?) OR lower(tag) = lower(?)', params[:demo][:entity], params[:demo][:entity]], :limit => 10)
+            c.each { |clan| q<< " OR entity1_local_id = #{clan.id} OR entity2_local_id = #{clan.id}" }
+          end
+        else
           u = User.find_by_login(params[:demo][:entity])
           q<< " OR entity1_local_id = #{u.id} OR entity2_local_id = #{u.id}"
-        else
           c = Clan.find(:all, :conditions => ['lower(name) = lower(?) OR lower(tag) = lower(?)', params[:demo][:entity], params[:demo][:entity]], :limit => 10)
           c.each { |clan| q<< " OR entity1_local_id = #{clan.id} OR entity2_local_id = #{clan.id}" }
         end
-      else
-        u = User.find_by_login(params[:demo][:entity])
-        q<< " OR entity1_local_id = #{u.id} OR entity2_local_id = #{u.id}"
-        c = Clan.find(:all, :conditions => ['lower(name) = lower(?) OR lower(tag) = lower(?)', params[:demo][:entity], params[:demo][:entity]], :limit => 10)
-        c.each { |clan| q<< " OR entity1_local_id = #{clan.id} OR entity2_local_id = #{clan.id}" }
+        sql_conds<< "(#{q})"
       end
-      sql_conds<< "(#{q})"
-    end
-    
-    sql_conds<< "entity1_external = #{User.connection.quote(params[:demo][:entity_external])} OR entity2_external = #{User.connection.quote(params[:demo][:entity_external])}" if params[:demo][:entity_external]
+      
+      sql_conds<< "entity1_external = #{User.connection.quote(params[:demo][:entity_external])} OR entity2_external = #{User.connection.quote(params[:demo][:entity_external])}" if params[:demo][:entity_external]
     end
     sql_conds<< ['1 = 1']
     @demos = portal.demo.find(:published, :conditions => sql_conds.join(' AND '), :limit => 51, :order => 'created_on')
@@ -54,7 +54,7 @@ class DemosController < ArenaController
   def download
     @demo = Demo.find(params[:id])
     raise ActiveRecord::RecordNotFound unless @demo.is_public?
-      
+    
     @title = @demo.title
     @demo_mirrors = @demo.demo_mirrors
     Demo.increment_counter('downloaded_times', @demo.id)
@@ -63,21 +63,11 @@ class DemosController < ArenaController
     CacheObserver.expire_fragment("/common/demos/index/downloads_#{@demo.main_category.id}/page_*") # TODO MUY HEAVY, no podemos hacer que cada descarga suponga borrar todas las caches de índices
     CacheObserver.expire_fragment("/common/demos/index/most_demoed_#{@demo.main_category.root_id}")
     if params[:r] 
-        #if Cms::URL_REGEXP_FULL =~ params[:r] #DemoMirror.find_by_url(URI::unescape(params[:r]))
-          @demo_link = params[:r]
-        #lse
-        # flash[:error] = "URL de descarga inválida"
-        # redirect_to "/descargas/show/#{@demo.id}"
-        #nd
+      @demo_link = params[:r]
     else
-      # CacheObserver.expire_fragment("/common/descargas/most_downloaded_#{@demo.main_category.root_id}")
-	gm_link = @demo.created_on > 1.day.ago ? 0 : 1
-# TODO temp
-#gm_link = nil
-      final_mirror = nil # Demo.create_symlink(dd.download_cookie, @demo.file, gm_link)  # 1 = NLS
-      final_mirror = 0 if final_mirror.nil?
+      gm_link = @demo.created_on > 1.day.ago ? 0 : 1
       end_file = @demo.file.to_s.gsub("#{RAILS_ROOT}/public/storage", '')
-      @demo_link = @demo.file.to_s # "#{Download::MIRRORS_DOWNLOAD[final_mirror]}d/#{dd.download_cookie}/#{File.basename(end_file)}" 
+      @demo_link = @demo.file.to_s 
     end
     render :layout => 'blank'
   end
