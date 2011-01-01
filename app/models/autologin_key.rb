cea3e10949d7e09b0c3f0808680153fa2c7546a5
slnc <s@slnc.me>
2011-01-01 19:58:40 +0100

    More refactorings

--- app/models/autologin_key.rb
+++ app/models/autologin_key.rb
@@ -1,3 +1,10 @@
 class AutologinKey < ActiveRecord::Base
   belongs_to :user
+  
+  def touch
+    # Things to do when an autologin key is used.
+    tstamp = Time.now
+    self.user.update_attribute(:lastseen_on, tstamp)
+    self.update_attribute(:lastused_on, tstamp)
+  end
 end
