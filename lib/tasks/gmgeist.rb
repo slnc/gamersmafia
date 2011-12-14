e9cdcf2fcfc3a1533701b6f8f67440168107ce06
slnc <s@slnc.me>
2011-12-14 21:25:57 +0100

    Añadidos scripts para procesar resultados de gmgeist

---
+++ lib/tasks/gmgeist.rb
@@ -0,0 +1,17 @@
+namespace :gm do
+  desc "GmGeist Warning 2011"
+  task :gmgeist2011 => :environment do
+    subject = '¡Sólo quedan 2 días para que concluya la GmGeist 2011!'
+    body = '''Si todavía no has rellenado la primera encuesta anual de Gamersmafia esta es tu oportunidad:
+
+    http://gamersmafia.com/noticias/show/45704
+
+    Tu opinión es imprescindible para poder decidir el rumbo de la comunidad.
+    ¡Muchas gracias!
+    '''
+    nagato = User.find_by_login('nagato')
+    recipient = User.find(1)
+    m = Message.new(:title => subject, :sender => nagato, :recipient => recipient, :message => body)
+    m.save
+  end
+end
