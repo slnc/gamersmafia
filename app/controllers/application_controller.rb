bc859693600f028429edc3cc6e7eaebd4cf993ec
slnc <s@slnc.me>
2012-05-27 11:23:41 +0200

    Añadidos controles para params[:page] inválidos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -214,6 +214,7 @@ Request information:
 
   # Used to be able to leave out the action
   def gm_process
+    params[:page] = 1 if params.has_key?(:page) && params[:page].to_i < 1
     @madness = true
     @_track_done = false
     seconds = Benchmark.realtime do
