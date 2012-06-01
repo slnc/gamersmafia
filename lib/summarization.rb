c1813b71d60e90182e727acbe7cfc9ea9c65017f
slnc <s@slnc.me>
2012-06-01 10:05:15 +0200

    Cleanup and added tests to summarization

--- lib/summarization.rb
+++ lib/summarization.rb
@@ -1,88 +1,325 @@
 module Summarization
-  class Sentence
-    def initialize(sentence)
-      @sentence = sentence
-    end
-  end
-
-  def self.summarize(content, desired_summary_word_length)
-    sentences = self.extract_sentences(content)
-    sentences_graph = self.build_graph_from_sentences(sentences)
-    self.compute_pairwise_similarity(sentences_graph)
-    self.compute_ranks(sentences_graph)
-    self.build_summary(sentences_graph, desired_summary_word_length)
-  end
-
-  def self.extract_sentences(object)
-    case object.class.name
-    when 'String':
-      self.extract_sentences_from_string(object)
-    else  # Assume it's an ActsAsContent object
-      self.extract_sentences_from_content(object)
-    end
-  end
-
-  def self.extract_sentences_from_content(content)
-    sentences = []
-    sentences<< self.extract_sentences(content.title)
-    if content.respond_to?(:description)
-      sentences<< self.get_all_text(content.description)
-    end
-    if content.respond_to?(:main)
-      sentences<< self.extract_sentences(content.main)
-    end
-
-    content.comments.each do |comment|
-      sentences<< self.extract_sentences(comment.comment)
-    end
-  end
-
-  def self.extract_sentences_from_string(some_string)
-    some_string.split("<br />")
-  end
-
   WINDOW = 10
+
+  if nil
+    def self.summarize(content, desired_summary_word_length)
+      sentences = self.extract_sentences(content)
+      sentences_graph = self.build_graph_from_sentences(sentences)
+      self.compute_pairwise_similarity(sentences_graph)
+      self.compute_ranks(sentences_graph)
+      self.build_summary(sentences_graph, desired_summary_word_length)
+    end
+
+    def self.extract_sentences(object)
+      case object.class.name
+      when 'String':
+        self.extract_sentences_from_string(object)
+      else  # Assume it's an ActsAsContent object
+        self.extract_sentences_from_content(object)
+      end
+    end
+
+    def self.extract_sentences_from_content(content)
+      sentences = []
+      sentences<< self.extract_sentences(content.title)
+      if content.respond_to?(:description)
+        sentences<< self.get_all_text(content.description)
+      end
+      if content.respond_to?(:main)
+        sentences<< self.extract_sentences(content.main)
+      end
+
+      content.comments.each do |comment|
+        sentences<< self.extract_sentences(comment.comment)
+      end
+    end
+
+    def self.extract_sentences_from_string(some_string)
+      some_string.split("<br />")
+    end
+  end
+
   def self.summarize_text(some_text)
     words = self.tokenize(some_text)
     graph = self.build_graph(words)
+    target_keywords = graph.size / 2
     self.add_cooccurrence_relations(words, graph, Summarization::WINDOW)
-    puts "debugging..."
     self.converge(graph)
-    graph.each do |word, node|
-      puts node
-    end
     out = graph.sort_by { |word, node| -node.score }
-    out.collect {|word, node| [word, node.score]}
+    ranked_words = out.collect {|word, node| [word, node.score]}
+    final_out = []
+    target_keywords.times do |i|
+      final_out.append(ranked_words[i])
+    end
+    final_out
+    merged_keywords = self.merge_adjacent_keywords(
+        words, final_out.collect {|word, score| word})
   end
 
   ARTICLES_ADVERBS_VERBS = [
-     "of",
-     "and",
-     "the",
-     "a",
-     "for",
-     "all",
-     "considered",
-     "types",
-     "used",
-     "in",
-     "solving",
-     "are",
-     "supporting",
-     "constructing",
-     "can",
-     "given",
-     "be",
-     "over",
-     "corresponding",
-     "these",
-     "generating",
-     "upper",
-     "mixed",
+ "a",
+ "all",
+ "and",
+ "are",
+ "be",
+ "can",
+ "considered",
+ "constructing",
+ "corresponding",
+ "for",
+ "generating",
+ "given",
+ "in",
+ "mixed",
+ "of",
+ "over",
+ "solving",
+ "supporting",
+ "the",
+ "these",
+ "types",
+ "upper",
+ "used",
+
+ # SPANISH
+ "abrimos",
+ "acuerdo",
+ "agolpara",
+ "aguantó",
+ "ahorrarte",
+ "ahorrarte",
+ "al",
+ "algo",
+ "allí",
+ "ansiábamos",
+ "ante",
+ "anunciado",
+ "aparece",
+ "apaña",
+ "armado",
+ "asi",
+ "así",
+ "aun",
+ "aunque",
+ "bajarse",
+ "bajo",
+ "bastante",
+ "bien",
+ "cambiar",
+ "cambiarla",
+ "casi",
+ "cito",
+ "con",
+ "congelarse",
+ "conmigo",
+ "constantemente",
+ "contará",
+ "contemplaban",
+ "continuas",
+ "convertido",
+ "creo",
+ "cuenta",
+ "da",
+ "de",
+ "del",
+ "den",
+ "desarrollado",
+ "desde",
+ "dios",
+ "disponible",
+ "divertían",
+ "el",
+ "el",
+ "ella",
+ "ellos",
+ "ellos",
+ "empieza",
+ "en",
+ "encabeza",
+ "encuentra",
+ "entre",
+ "es",
+ "eso",
+ "estará",
+ "estas",
+ "este",
+ "esto",
+ "estoy",
+ "evitar",
+ "fue",
+ "fueran",
+ "funciona",
+ "funcionar",
+ "funcione",
+ "funciono",
+ "guardar",
+ "ha",
+ "hacer",
+ "hacerse",
+ "han",
+ "hará",
+ "hasta",
+ "hay",
+ "haya",
+ "he",
+ "hecho",
+ "hechos",
+ "hice",
+ "hola",
+ "importante",
+ "ir",
+ "iria",
+ "joder",
+ "jugaba",
+ "la",
+ "lanzado",
+ "lanzando",
+ "lanzó",
+ "las",
+ "le",
+ "llamar",
+ "lleva",
+ "llevara",
+ "lo",
+ "los",
+ "marca",
+ "marginados",
+ "mas",
+ "me",
+ "mejor",
+ "mejorado",
+ "mete",
+ "mi",
+ "mientras",
+ "misma",
+ "mismo",
+ "modo",
+ "momento",
+ "momento",
+ "mucho",
+ "más",
+ "nada",
+ "nadie",
+ "ninguna",
+ "nisu",
+ "no",
+ "nosotros",
+ "nueva",
+ "o",
+ "ocupaba",
+ "pagar",
+ "par",
+ "para",
+ "parar",
+ "pero",
+ "personaliazble",
+ "peta",
+ "poco",
+ "pocos",
+ "poner",
+ "por desgracia",
+ "por",
+ "porque",
+ "posee",
+ "primera",
+ "pronto",
+ "pueda",
+ "puedo",
+ "pues",
+ "que",
+ "queden",
+ "quería",
+ "quiero",
+ "realizada",
+ "rechazados",
+ "reiniciarse",
+ "responde",
+ "sabido",
+ "se",
+ "sea",
+ "segunda",
+ "ser",
+ "si",
+ "siguiente",
+ "sistema",
+ "situada",
+ "sobre",
+ "solo",
+ "su",
+ "subir",
+ "sus",
+ "sustituirlo",
+ "tan",
+ "tanto",
+ "tarde",
+ "te",
+ "tengas",
+ "tengo",
+ "tenéis",
+ "tiene",
+ "tienes",
+ "tienes",
+ "tirando",
+ "todo",
+ "todos",
+ "tras",
+ "través",
+ "tu",
+ "tuvimos",
+ "un",
+ "una",
+ "unas",
+ "unos",
+ "unos",
+ "vale",
+ "vaya",
+ "veces",
+ "venirse",
+ "vez",
+ "vuelve",
+ "y",
+ "ya",
+ "yo",
   ]
+
+  # TODO(slnc): proper POS Tagger
 
   MAX_ITERATIONS = 30
   CONVERGENCE_LIMIT = 0.0001
+
+  class Node
+    attr_accessor :score, :neighbors
+    attr_reader :word
+
+    def initialize(word)
+      @word = word
+      @neighbors = []
+      @score = 1.0
+    end
+
+    def to_s
+      "#{@word} #{@score} #{@neighbors.size}"
+    end
+
+    # Two nodes are considered equal if they have the same word, the same score
+    # and the same number of neighbors.
+    def ==(another_node)
+      if self.word != another_node.word || self.score != another_node.score
+        return false
+      end
+
+      # We compare neighbors
+      ours = self.neighbors.collect { |neighbor| neighbor.to_s }
+      theirs = another_node.neighbors.collect { |neighbor| neighbor.to_s }
+      ours.sort == theirs.sort
+    end
+
+    def bidirectional_link(neighbor)
+      return if neighbor == self
+      self.neighbors<< neighbor if !self.neighbors.include?(neighbor)
+      neighbor.neighbors<< self if !neighbor.neighbors.include?(self)
+    end
+  end
 
   def self.converge(graph, d=0.85)
     iterations = 0
@@ -135,6 +372,7 @@ module Summarization
     1.0
   end
 
+  private
   def self.add_cooccurrence_relations(words, graph, window)
     outer_idx = 0
     puts ">>>>>>>>>>> ADDDING COOCURRENCE RELATIONSHIPS"
@@ -176,27 +414,30 @@ module Summarization
   end
 
   def self.tokenize(some_text)
-    some_text.downcase.gsub(".", " ").gsub(",", " ").gsub(/[ ]{2,}/, " ").split(" ")
+    some_text.downcase.gsub(".", " ").gsub(",", " ").gsub(/[^a-zA-ZáéíóúñÁÉÍÓÚÑ]{2,}/, " ").split(" ")
   end
 
-  class Node
-    attr_accessor :score, :neighbors
-    attr_reader :word
-
-    def initialize(word)
-      @word = word
-      @neighbors = []
-      @score = 1.0
+  def self.merge_adjacent_keywords(words, target_keywords)
+    merged_keyword = true
+    copied_target_kws = target_keywords.dup
+    puts "#{copied_target_kws} vs #{target_keywords}"
+    while merged_keyword
+      merged_keyword = false
+      i = 0
+      (words.size - 1).times do |i|
+        if copied_target_kws.include?(words[i]) && copied_target_kws.include?(words[i+1])
+          copied_target_kws.delete(words[i])
+          copied_target_kws.delete(words[i+1])
+          new_kw = "#{words[i]} #{words[i+1]}"
+          copied_target_kws<< new_kw
+          words[i+1] = new_kw
+          puts " going to delete #{words[i]} and replace with #{new_kw}"
+          words.delete(i)
+          merged_keyword = true
+          break
+        end
+      end
     end
-
-    def to_s
-      "#{@word} #{@score} #{@neighbors.size}"
-    end
-
-    def bidirectional_link(neighbor)
-      return if neighbor == self
-      self.neighbors<< neighbor if !self.neighbors.include?(neighbor)
-      neighbor.neighbors<< self if !neighbor.neighbors.include?(self)
-    end
+    copied_target_kws
   end
 end
--- test/lib/summarization_test.rb
+++ test/lib/summarization_test.rb
@@ -15,6 +15,135 @@ class SummarizationTest < ActiveSupport::TestCase
   systems and systems of mixed types.
   END
 
+  PAPER_KEYPHRASE_EXTRACTION = <<-END
+  505 Games y la desarrollado Behaviour Interactive han anunciado la llegada de un nuevo Naughty Dog que estará disponible en Xbox Live Arcade y Playstation Network a finales de este año.
+
+  Naughty Bear: Panic in Paradise mete a los jugadores en la piel de Naughty Bear, que una vez más vuelve a ser uno de los habitantes más rechazados y marginados de todo el vecindario. Su viaje le llevara hasta unas paradisiacas vacciones de lujo en la llamada Paradise Island, pero por desgracia nadie le ha invitado a venirse de vacaciones. Armado hasta los dientes, este Oso hará pagar a todos que le den la espalda, uno por uno.
+
+  Naughty Bear: Panic in Paradise contará con un mejorado sistema de combate, nuevo sistema para subir de nivel, equipo personaliazble y más de 30 misiones en 11 diferentes áreas.
+  END
+
+PAPER_KEYPHRASE_EXTRACTION = <<-END
+La fotografía que tenéis sobre estas líneas fue realizada el lunes en las oficinas centrales que VK.com, la red social más popular de Rusia, Ucrania, Bielorrusia, Kazajistán y Moldavia y una de las 50 páginas más populares de Internet de acuerdo al ránking Alexa, posee en San Petersburgo.
+
+El diseño de este portal, que fue lanzado en septiembre de 2006, copia de manera poco disimulada el aspecto y las funcionalidades de Facebook, pero ello no ha sido óbice para que se haya convertido en un enorme éxito y haya hecho de su fundador, un joven que responde al nombre de Pavel Durov, un multimillonario con tan solo 27 años.
+
+
+Tristemente, da la sensación de que no ha sabido asimilar la montaña de dinero y fama que este portal le ha reportado. La imagen que encabeza este artículo así lo atestigua. En la misma aparece junto a su vicepresidente lanzando a modo de juego avioncitos hechos con billetes de 5.000 rublos (unos 122 euros al cambio actual) a la gente que se encuentra en la calle situada bajo su despacho.
+
+
+
+La lluvia de dinero en forma de aviones de papel provocó que una muchedumbre se agolpara en unos pocos metros cuadrados y que se produjeran peleas continuas por hacerse con el preciado dinero entre los vecinos y transeúntes que allí se encontraban mientras Durov y sus acompañantes se divertían ante el inusual y denigrante espectáculo que contemplaban desde sus privilegiados aposentos.
+
+En un rato lanzó por la ventana unos 1.800 euros. Más tarde, a través de su cuenta de Twitter, confesó en un alarde de soberbia insultante, que, y cito textualmente, "tuvimos que parar pronto debido a que la gente se comportó como si fueran animales". Todo un personaje, como podéis ver.
+END
+
+PAPER_KEYPHRASE_EXTRACTION = <<-END
+Hola Mafiosos,
+
+Tras días de hard work con trinee y V1rus_92 por fin abrimos las puertas del server Towny NO PREMIUM que tanto ansiábamos todos.
+
+El servidor consta de los siguiente Mods:
+
+- BuildCraft 2.2.14
+
+- IndustrialCraft 2
+
+- Aditional Pipes 2.1.3
+
+- Advanced Machines 4.0
+
+- BuildCraft & IndustrialCraft CrossOver 1.28
+
+- Compact Solars 2.2.0.5
+
+- Redpower 2 (completo)
+
+Y de los siguientes plugins:
+
+- bLift (Ascensores)
+
+- ChestShop (Tienda)
+
+- Crazy Login (Cuentas)
+
+- Crazy Core (para que funcione Crazy Login ¿x'D?)
+
+- Essentials (Comandos básicos para servers)
+
+- iConomy (Sistema de economía)
+
+- mcMMO (Niveles)
+
+- MyWorlds (Distintos Mundos)
+
+- NoLagg (Antilag)
+
+- PermissionsEX (Sistema de Permisos)
+
+- Player Tracker (Capo aka detector de Multicuentas)
+
+- Stargate (Portales entre mundos)
+
+- Towny
+
+- Questioner (Base de Towny)
+
+- WeatherMan (Biomas)
+
+- WorldEdit
+
+- Tree Assist (Treecapitator que replanta)
+
+Estamos ansiosos de que este proyecto se expanda por la comunidad y pasemos un buen rato entre todos. Podremos empezar a dar guerra el Lunes 28 de Mayo.
+
+La IP del servidor es:  212.227.22.192:8008
+
+Mapa del servidor: http://212.227.22.192:8009/
+
+TeamSpeak del servidor: 212.227.22.192:9999
+
+Para entrar al servidor, hay que bajarse ESTE archivo y sustituirlo por tu archivo .minecraft. Para ello debes ir a inicio>ejecutar>escribir %appdata% y cambiar la carpeta del .rar por la de tu pc para que todos los mods se queden en tu ordenador. Al hacer esto, perderás las demás ip que tengas guardadas, y el single player, para no perderlas, recomendamos guardar tu carpeta .minecraft en otra parte para poder intercambiarlas.
+
+
+
+Imágenes del spawn en proceso:
+END
+
+PAPER_KEYPHRASE_EXTRACTION = <<-END
+Hola ninios y ninias.
+
+Creo que mi tarjeta grafica ha muerto. El PC se peta constantemente y al reiniciarse solo me aparece un pantallazo pixelado de lo último que ocupaba la pantalla al congelarse.
+
+La actual es una Gforce 8600Gt...El procesador es un quad...que le puedo poner que sea de su talla, y lo mas importante, se lo puedo hacer yomismo o tengo que llamar a mi vecino de 12 años para no joder nada?
+empieza poniendo la pasta que te quieres gastar, de cuanto, de que marca y de que modelo es tu fuente
+
+La pasta, no es problema pero no quiero gastar mucho.
+
+La fuente es una nisu de 450 W
+
+Quería evitar cambiarla porque esto tiene mas cables que dios, pero si no hay mas remedio...
+
+
+pues si no quieres cambiar la fuente y quieres ahorrarte pasta http://www.pccomponentes.com/sapphire_radeon_hd_4850_1gb_gddr3_refurbished.html
+
+Aunque la fuente iria justisima, si quieres algo mas potente www.pccomponentes.com/sapphire_radeon_hd_6850_1gb_gddr5.html + http://www.pccomponentes.com/tacens_radix_v_650w.html
+
+
+Mete la grafica al horno!
+
+
+
+#4 yo he hecho eso del horno con la hd4650 y funciono, pero por poco tiempo, unos 5 dias. Es un metodo temporal y aun asi no tienes ninguna garantia de que vaya a funcionar.
+
+
+#5 el metodo profesional es hacer reballing, que vale casi lo mismo que la grafica nueva, aun asi, con la pistola de calor si se apaña bastante bien, mucho mejor que con el horno
+
+
+Yo lo del horno lo hice ya 2 veces con la 8800GTS 512, la primera vez me aguantó un par de meses, y la segunda lleva de momento 3 semanas, y mientras pueda ir tirando así mejor que mejor, que estoy peladísimo.
+
+END
+
   PAPER_KEYPHRASE_EXPECTED_SORTED = [
       'linear constraints',
       'linear diophantine equations',
@@ -30,12 +159,53 @@ class SummarizationTest < ActiveSupport::TestCase
         Summarization.summarize_text(SummarizationTest::PAPER_KEYPHRASE_EXTRACTION))
   end
 
-
   test "build_graph" do
-    assert_equal %w(hello world), Summarization.build_graph(["hello", "world"])
+    node_hello = Summarization::Node.new("hello")
+    node_world = Summarization::Node.new("world")
+    expected_graph = {"hello" => node_hello, "world" => node_world}
+    assert_equal(
+        expected_graph, Summarization.send(:build_graph, ["hello", "world"]))
+  end
+
+  test "node bidirectional_link" do
+    node_hello = Summarization::Node.new("hello")
+    node_world = Summarization::Node.new("world")
+    assert_equal 0, node_world.neighbors.size
+    assert_equal 0, node_hello.neighbors.size
+    node_hello.bidirectional_link(node_world)
+    assert_equal [node_hello], node_world.neighbors
+    assert_equal [node_world], node_hello.neighbors
   end
 
   test "tokenize" do
-    assert_equal %w(hello world), Summarization.tokenize("hello world")
+    assert_equal %w(), Summarization.send(:tokenize, "")
+    assert_equal %w(hello world), Summarization.send(:tokenize, "hello world")
+    assert_equal %w(hello world), Summarization.send(:tokenize, "hello.world")
+    assert_equal %w(hello-world), Summarization.send(:tokenize, "hello-world")
+  end
+
+  test "add_cooccurrence_relations simple" do
+    # Build input_graph
+    node_hello = Summarization::Node.new("hello")
+    node_world = Summarization::Node.new("world")
+    input_graph = {"hello" => node_hello, "world" => node_world}
+
+    # Build expected_graph
+    node_hello = Summarization::Node.new("hello")
+    node_world = Summarization::Node.new("world")
+    node_hello.neighbors<< node_world
+    node_world.neighbors<< node_hello
+    expected_graph = {"hello" => node_hello, "world" => node_world}
+
+    actual_graph = Summarization.send(
+        :add_cooccurrence_relations, ["hello", "world"], input_graph, 10)
+
+    assert_equal(expected_graph, actual_graph)
+  end
+
+  test "merge_adjacent_keywords" do
+    merged_kws = Summarization.send(
+      :merge_adjacent_keywords, %w(hello world foo bar), %w(hello world bar))
+    assert_equal ["hello world", "bar"], merged_kws
   end
 end
