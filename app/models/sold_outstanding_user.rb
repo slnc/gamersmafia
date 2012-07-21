1e68a7c8f355f0144c792bffc9415de7e25ec29b
slnc <s@slnc.me>
2012-07-22 01:32:36 +0200

    Formateo SoldOutstandingUser

--- app/models/sold_outstanding_user.rb
+++ app/models/sold_outstanding_user.rb
@@ -2,8 +2,20 @@
 class SoldOutstandingUser < SoldProduct
   # options debe tener portal_id
   def _use(options)
-    oe = OutstandingEntity.factory(options[:portal_id], 'OutstandingUser', user_id, 'Soborno a las altas esferas')
-    Message.create({:user_id_to => self.user_id, :user_id_from => User.find_by_login('nagato').id, :title => "Fecha de publicación de tu compra de \"Usuario Destacado\"", :message => "El producto \"Usuario destacado\" que acabas de comprar estará activo durante todo el día #{oe.active_on.strftime('%d de %B de %Y')} en portada de #{oe.portal.name}."})
+    oe = OutstandingEntity.factory(
+        options[:portal_id],
+        'OutstandingUser',
+        user_id,
+        'Soborno a las altas esferas')
+    publish_date = oe.active_on.strftime('%d de %B de %Y')
+    Message.create({
+        :user_id_to => self.user_id,
+        :user_id_from => User.find_by_login('nagato').id,
+        :title => "Fecha de publicación de tu compra de \"Usuario Destacado\"",
+        :message => (
+            "El producto \"Usuario destacado\" que acabas de comprar estará" +
+            " activo durante todo el día #{publish_date} en portada de" +
+            " #{oe.portal.name}."})
     true
   end
 end
