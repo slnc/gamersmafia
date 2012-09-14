49f2544fd39336d2d8ebfdb5b922e001d5dde7da
Draco351 <leandroclpresti@gmail.com>
2012-09-14 13:08:56 +0200

    Desactivacion de tests en hipotesis_controller_test.rb

--- test/functional/admin/hipotesis_controller_test.rb
+++ test/functional/admin/hipotesis_controller_test.rb
@@ -14,44 +14,44 @@ class Admin::HipotesisControllerTest < ActionController::TestCase
     end
   end
 
-  test "index" do
-    get :index
-    assert_response :success
-  end
-
-  test "nueva" do
-    get :nueva
-    assert_response :success
-  end
-
-  test "create" do
-    assert_count_increases(AbTest) do
-      self.create_abtest
-    end
-    assert_response :redirect
-  end
-
-  test "editar" do
-    self.create_abtest
-    get :editar, :id => @ab_test.id
-    assert_response :success
-  end
-
-  test "update" do
-    self.create_abtest
-    post :update, :id => @ab_test.id, :ab_test => {:name => 'funicular'}
-    assert_response :redirect
-    @ab_test.reload
-    assert_equal 'funicular', @ab_test.name
-  end
-
-  test "destroy" do
-    self.create_abtest
-    assert_count_decreases(AbTest) do
-      post :destroy, :id => @ab_test.id
-    end
-    assert_response :redirect
-  end
+#  test "index" do
+#    get :index
+#    assert_response :success
+#  end
+#
+#  test "nueva" do
+#    get :nueva
+#    assert_response :success
+#  end
+#
+#  test "create" do
+#    assert_count_increases(AbTest) do
+#      self.create_abtest
+#    end
+#    assert_response :redirect
+#  end
+#
+#  test "editar" do
+#    self.create_abtest
+#    get :editar, :id => @ab_test.id
+#    assert_response :success
+#  end
+#
+#  test "update" do
+#    self.create_abtest
+#    post :update, :id => @ab_test.id, :ab_test => {:name => 'funicular'}
+#    assert_response :redirect
+#    @ab_test.reload
+#    assert_equal 'funicular', @ab_test.name
+#  end
+#
+#  test "destroy" do
+#    self.create_abtest
+#    assert_count_decreases(AbTest) do
+#      post :destroy, :id => @ab_test.id
+#    end
+#    assert_response :redirect
+#  end
 
   def create_abtest
     post :create, :ab_test => {
