031fa8abbefea27909731ae745df209d9f744a17
Draco351 <leandroclpresti@gmail.com>
2012-09-15 15:04:56 +0200

    Refactorizacion de categorias_controller.rb

--- app/controllers/admin/categorias_controller.rb
+++ app/controllers/admin/categorias_controller.rb
@@ -8,11 +8,22 @@ class Admin::CategoriasController < ApplicationController
     if params[:id]
       t = Term.find_by_id(params[:id])
       if t && (params[:content_type] || t.taxonomy)
-        raise AccessDenied unless Cms::can_admin_term?(user, t, params[:content_type] ? params[:content_type] : Cms.extract_content_name_from_taxonomy(t.taxonomy))
+        raise AccessDenied unless Cms::can_admin_term?(
+            user,
+            t,
+            params[:content_type] ? params[:content_type] :
+            Cms.extract_content_name_from_taxonomy(t.taxonomy))
       end
     end
 
-    raise AccessDenied unless user.users_skills.count(:conditions => "role IN ('Boss', 'Underboss', 'Don', 'ManoDerecha', 'Sicario', 'Editor')") > 0 || user.has_admin_permission?(:capo)
+    raise AccessDenied unless user.users_skills.count(
+        :conditions => "role IN (
+            'Boss',
+            'Don',
+            'Editor',
+            'ManoDerecha',
+            'Sicario',
+            'Underboss')") > 0 || user.has_admin_permission?(:capo)
   end
 
   public
@@ -22,7 +33,9 @@ class Admin::CategoriasController < ApplicationController
 
   def index
     @title = "Categorías"
-    @navpath = [['Admin', '/admin'], ['Categorías de Contenidos', '/admin/categorias']]
+    @navpath = [
+        ['Admin', '/admin'],
+        ['Categorías de Contenidos', '/admin/categorias']]
     @categories = nil
     render :template => "/admin/categorias/index"
   end
@@ -65,7 +78,11 @@ class Admin::CategoriasController < ApplicationController
     # TODO permisos
     @term = Term.find(params[:id])
     dst = Term.find(params[:destination_term_id])
-    @term.find(:all, :content_type => params[:content_type], :conditions => "contents.id in (#{params[:contents].join(', ')})").each do |c|
+    @term.find(
+        :all,
+        :content_type => params[:content_type],
+        :conditions => "contents.id in "+
+                       "(#{params[:contents].join(', ')})").each do |c|
       @term.unlink(c.unique_content)
       dst.link(c.unique_content)
     end
@@ -79,9 +96,12 @@ class Admin::CategoriasController < ApplicationController
 
     if @term.can_be_destroyed?
       @term.destroy
-      flash[:notice] = "Categoría <strong>#{@term.name}(#{@term.taxonomy})</strong> destruída correctamente. Khali se complace."
+      flash[:notice] = "Categoría "+
+                       "<strong>#{@term.name}(#{@term.taxonomy})</strong> "+
+                       "destruída correctamente. Khali se complace."
     else
-      flash[:error] = "No se puede eliminar la categoría. Asegúrate de que no tiene subcategorías ni contenidos."
+      flash[:error] = "No se puede eliminar la categoría. "+
+                      "Asegúrate de que no tiene subcategorías ni contenidos."
     end
     redirect_to params[:redirto] ? params[:redirto] : '/admin/categorias'
   end
@@ -90,13 +110,17 @@ class Admin::CategoriasController < ApplicationController
     raise AccessDenied if params[:term][:taxonomy].to_s == ''
     @term = Term.new(params[:term])
     if @term.save
-      if !Cms.can_edit_term?(user, @term, Cms.extract_content_name_from_taxonomy(params[:term][:taxonomy]))
+      if !Cms.can_edit_term?(
+          user,
+          @term,
+          Cms.extract_content_name_from_taxonomy(params[:term][:taxonomy]))
         @term.destroy
         raise AccessDenied
       end
       flash[:notice] = 'Categoría creada correctamente.'
     else
-      flash[:error] = "Error al crear la categoría: #{@term.errors.full_messages_html}"
+      flash[:error] = "Error al crear la categoría: "+
+                      "#{@term.errors.full_messages_html}"
     end
     redirect_to params[:redirto] ? params[:redirto] : '/admin/categorias'
   end
