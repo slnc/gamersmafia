6f224a1e09193015fb857ea0fab80226b605477b
slnc <s@slnc.me>
2012-10-11 06:18:02 +0200

    Corrige: puntos de karma entre 1000 y 9999 no se muestran correctamente
    fixes #230

--- test/unit/helpers/miembros_helper_test.rb
+++ test/unit/helpers/miembros_helper_test.rb
@@ -12,4 +12,40 @@ class MiembrosHelperTest < ActionView::TestCase
     u1.emblems_mask = "1.0.0.0.1"
     assert_equal [["common", "1"], ["special", "1"]], user_emblem_stats(u1)
   end
+
+  test "draw_karma_bar_sm less than 1k" do
+    u1 = User.find(1)
+    u1.cache_karma_points = 900
+    assert draw_karma_bar_sm(u1).include?("900")
+  end
+
+  test "draw_karma_bar_sm thousands 1" do
+    u1 = User.find(1)
+    u1.cache_karma_points = 1000
+    assert draw_karma_bar_sm(u1).include?("1,000")
+  end
+
+  test "draw_karma_bar_sm thousands 2" do
+    u1 = User.find(1)
+    u1.cache_karma_points = 1234
+    assert draw_karma_bar_sm(u1).include?("1,234")
+  end
+
+  test "draw_karma_bar_sm tens of thousands1" do
+    u1 = User.find(1)
+    u1.cache_karma_points = 10000
+    assert draw_karma_bar_sm(u1).include?("10.0k")
+  end
+
+  test "draw_karma_bar_sm tens of thousands2" do
+    u1 = User.find(1)
+    u1.cache_karma_points = 10001
+    assert draw_karma_bar_sm(u1).include?("10.0k")
+  end
+
+  test "draw_karma_bar_sm tens of thousands3" do
+    u1 = User.find(1)
+    u1.cache_karma_points = 10100
+    assert draw_karma_bar_sm(u1).include?("10.1k")
+  end
 end
