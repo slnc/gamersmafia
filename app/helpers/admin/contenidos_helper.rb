a077ecaaca36bddf83e1d9d6af050ea1e0309b42
slnc <s@slnc.me>
2012-10-11 07:08:14 +0200

    Corrige: La barra de moderación se sale del espacio asignado
    Esto no corrige el problema de fondo (que se necesite más del 100% para publicar
    un contenido).
    
    fixes #234

--- app/helpers/admin/contenidos_helper.rb
+++ app/helpers/admin/contenidos_helper.rb
@@ -4,6 +4,8 @@ module Admin::ContenidosHelper
     points = PublishingDecision.find_sum_for_content(obj)
     h = (points > 0) ? 7 : 14
     a = (points > 0) ? 'left' : 'right'
+    points = -1 if points < -1
+    points = 1 if points > 1
     w = 51 + (points.abs * 50).to_i
     "<div class=\"modbar\" style=\"text-align: #{a};\"><img style=\"background-position: #{a} -#{h}px; width: #{w}px;\" src=\"/images/blank.gif\" title=\"#{(points.abs * 100).to_i}%\" /></div>"
   end
