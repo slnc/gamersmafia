77174de6be3076f12b60fefd1b60cb83d01d2ab2
slnc <s@slnc.me>
2012-11-18 00:58:07 +0100

    Corrige error js al añadir nueva librería de syntax highlighting

--- app/assets/javascripts/external/jquery.litelighter.js
+++ app/assets/javascripts/external/jquery.litelighter.js
@@ -168,3 +168,36 @@
     };
 
 })(jQuery);
+
+/*
+ * jQuery Litelighter
+ * By: Trent Richardson [http://trentrichardson.com]
+ *
+ * Copyright 2012 Trent Richardson
+ * Dual licensed under the MIT or GPL licenses.
+ * http://trentrichardson.com/Impromptu/GPL-LICENSE.txt
+ * http://trentrichardson.com/Impromptu/MIT-LICENSE.txt
+ *
+ * Here are the extra languages, I suggest take what you
+ * need and delete the rest in your application
+ */
+(function($){
+
+  /* jQuery Litelighter syntax definition for SQL */
+  $.litelighter.languages.sql = {
+    comment: { re: /(\-\-.*|\#.*)/g, style: 'comment' },
+    string: $.litelighter.languages.generic.string,
+    numbers: $.litelighter.languages.generic.numbers,
+    keywords: { re: /(?:\b)(select|insert|update|delete|where|from|set|create|alter|drop|values|and|or|order|by|group|having|view|table|function|procedure|return|begin|end|with|as|into|false|true|null)(?:\b)/gi, style: 'keyword' },
+    operators: $.litelighter.languages.generic.operators
+  };
+
+  /* jQuery Litelighter syntax definition for php */
+  $.litelighter.languages.php = $.extend({},$.litelighter.languages.generic);
+
+  /* jQuery Litelighter syntax definition for php embedded in html */
+  $.litelighter.languages.htmlphp = $.extend({},$.litelighter.languages.html, {
+    php: { re: /(?:\&lt;\?php)([\s\S]+?)(?:\?\&gt;)/gi, language: 'php'}
+  });
+
+})(jQuery);
