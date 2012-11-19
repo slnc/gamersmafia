c852d40a2e98d862f01948ee8fd7b305e39736b5
slnc <s@slnc.me>
2012-11-19 01:05:59 +0100

    Correcciones a bugs varios causados por últimos cambios

--- db/migrate/20121118173210_migrate_quicklinks_to_interests.rb
+++ db/migrate/20121118173210_migrate_quicklinks_to_interests.rb
@@ -1,13 +1,15 @@
 class MigrateQuicklinksToInterests < ActiveRecord::Migration
   def up
-    UsersPreference.find(:all, :conditions => "name = 'quicklinks").each do |upref|
+    UsersPreference.find(:all, :conditions => "name = 'quicklinks'").each do |upref|
+      next if upref.value.to_s == ""
+      puts upref.value
       upref.value.each do |qlink|
-        t = Term.with_taxonomies(%s(Game GamingPlatform BazarDistrict Homepage)).find_by_slug(qlink[:code])
+        t = Term.with_taxonomies(%w(Game GamingPlatform BazarDistrict Homepage)).find_by_slug(qlink[:code])
         if t
-          if upref.user.create_interest(:entity_type_class => "Term", :entity_id => t.id)
-            puts "Migrated quicklink #{t.name} for #{upref.login}"
+          if upref.user.user_interests.create(:entity_type_class => "Term", :entity_id => t.id)
+            puts "Migrated quicklink #{t.name} for #{upref.user.login}"
           else
-            puts "Error migrating quicklink #{t.name} for #{upref.login}"
+            puts "Error migrating quicklink #{t.name} for #{upref.user.login}"
           end
         end
       end
