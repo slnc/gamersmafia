50432165a6f08df936e44ae783503b7827a52863
slnc <s@slnc.me>
2013-03-18 00:40:28 +0100

    Añadido warning sobre uso de 'decide' como método

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -9,6 +9,7 @@ class DecisionsController < ApplicationController
     render :layout => false # !self.request.xhr?
   end
 
+  # Do NOT use 'decide', as of Rails 3.2.10 it's a reserved keyword.
   def make_decision
     @decision = Decision.find(params[:id])
     decision_choice = DecisionChoice.find(params[:final_decision_choice].to_i)
