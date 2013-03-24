7ceb018a6e85b7a0531b6aa209abdbc3317c9e01
Draco351 <leandroclpresti@gmail.com>
2013-03-24 17:19:16 +0100

    Sintetiza función has_skill

--- app/models/user.rb
+++ app/models/user.rb
@@ -809,11 +809,7 @@ class User < ActiveRecord::Base
   end
 
   def has_skill?(skill)
-    if !self.users_skills.find(:first, :conditions => ['role = ?', skill]).nil?
-      true
-    else
-      false
-    end
+    !self.users_skills.find(:first, :conditions => ['role = ?', skill]).nil?
   end
 
   def has_any_skill?(skills)
