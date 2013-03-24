93373a89f1104a43e245904dbe9624b5f50b8d92
slnc <s@slnc.me>
2013-03-24 18:46:22 +0100

    Elimina automáticamente información sobre emails leídos para liberar espacio
    Ahora mismo no estamos haciendo nada con esta información y tenemos más de 1M de
    registros (todos los emails enviados desde 2008).

--- test/unit/sent_email_test.rb
+++ test/unit/sent_email_test.rb
@@ -2,7 +2,27 @@
 require 'test_helper'
 
 class SentEmailTest < ActiveSupport::TestCase
-  test "truth" do
-    assert true
+
+  def create_email
+    se = SentEmail.new(:message_key => "foo")
+    assert_difference("SentEmail.count") do
+      se.save
+    end
+    se
+  end
+
+  test "remove_old_sent_emails old" do
+    se = create_email
+    se.update_attribute(:created_on, 2.months.ago)
+    assert_difference("SentEmail.count", -1) do
+      SentEmail.remove_old_sent_emails
+    end
+  end
+
+  test "remove_old_sent_emails not old" do
+    se = create_email
+    assert_difference("SentEmail.count", 0) do
+      SentEmail.remove_old_sent_emails
+    end
   end
 end
